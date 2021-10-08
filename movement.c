24062e35a81812da9d6a54eb34208a1b7d5ad09e
Artemis Hingston <artemis.hingston@protonmail.com>
2021-10-09 11:41:48 +1300

    basis for showing the map on the led screen

---
+++ movement.c
@@ -0,0 +1,80 @@
+#include "system.h"
+#include "pio.h"
+#include "pacer.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+static uint8_t prev_column = 0;
+
+/** Define PIO pins driving LED matrix rows.  */
+static const pio_t rows[] =
+{
+    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO, 
+    LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
+    LEDMAT_ROW7_PIO
+};
+
+
+/** Define PIO pins driving LED matrix columns.  */
+static const pio_t cols[] =
+{
+    LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
+    LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
+};
+
+// TODO: Need to grab the maps here
+
+// static const uint8_t bitmap[] =
+// {
+//     ~0x30, ~0x46, ~0x40, ~0x46, ~0x30
+// };
+
+
+
+static void display_column (uint8_t row_pattern, uint8_t current_column)
+{
+    pio_output_high (cols[prev_column]);
+
+    for (uint8_t i = 0; i < LEDMAT_ROWS_NUM; i++) {
+        if ((row_pattern >> i) & 1) {
+            pio_output_low (rows[i]);
+        } else {
+            pio_output_high (rows[i]);
+        }
+    }
+
+    pio_output_low (cols[current_column]);
+    prev_column = current_column;   
+}
+
+
+int main (void)
+{
+    uint8_t current_column = 0;
+  
+    system_init ();
+    pacer_init (500);
+    
+    /* Initialise LED matrix pins. */
+    for (uint8_t i = 0; i < LEDMAT_ROWS_NUM; i++) {
+        pio_config_set (rows[i], PIO_OUTPUT_HIGH);
+    }
+
+    for (uint8_t j = 0; j < LEDMAT_COLS_NUM; j++) {
+        pio_config_set (cols[j], PIO_OUTPUT_HIGH);
+    }
+
+    while (1)
+    {
+        pacer_wait ();
+        
+        display_column (bitmap[current_column], current_column);
+    
+        current_column++;
+    
+        if (current_column > (LEDMAT_COLS_NUM - 1))
+        {
+            current_column = 0;
+        }           
+    }
+}
