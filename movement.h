61a71f51563a9fd87271c489e6eefe27bf92dd1a
Robert Beck <rbe78@uclive.ac.nz>
2021-10-16 13:07:50 +1300

    Update movement.h

--- movement.h
+++ movement.h
@@ -13,7 +13,7 @@
 bool is_legal_move (uint16_t map[], uint16_t player_X, uint16_t player_Y);
 
 /* Updates and displays the new map using coords */
-void update_map(uint16_t map[], uint16_t player_X, uint16_t player_Y);
+void update_map(uint16_t collectables[], uint16_t map[], uint16_t player_X, uint16_t player_Y);
 
 /* Allows the player to navigate around the map */
 void movement_update(uint16_t map[], Stats_t* levelStats);
