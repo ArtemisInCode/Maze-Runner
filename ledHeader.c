e8c04dcecdf8a14a075635f979d4089358aeef40
Robert Beck <rbe78@uclive.ac.nz>
2021-10-18 17:36:09 +1300

    Update ledHeader.c

--- ledHeader.c
+++ ledHeader.c
@@ -8,6 +8,7 @@
 
 void header_init(void)
 {
+	/* Sets all header pins to output */
 	for(uint8_t i = 1; i<7; i++) {
 		DDRD |= (1<<i);
 	}
@@ -16,11 +17,11 @@ void header_init(void)
 void header_set(uint8_t amount)
 {
 	if(amount < 7) {
-		for(uint8_t i = 1; i<7; i++) {
+		for(uint8_t i = 1; i<7; i++) { // Increments through each header pin
 			if(i<=amount) {
-				PORTD |= (1<<(7-i));
+				PORTD |= (1<<(7-i)); // Sets header pin HIGH
 			} else {
-				PORTD &= ~(1<<(7-i));
+				PORTD &= ~(1<<(7-i)); // Sets header pin LOW
 			}
 		}
 	}
