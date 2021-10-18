e5c27cab7c0365e359e3408c907d8830ab700bac
Artemis Hingston <irh20@uclive.ac.nz>
2021-10-18 16:53:47 +1300

    Update maps.h - changed struct names and added comments

--- maps.h
+++ maps.h
@@ -9,34 +9,39 @@
 
 #include <stdint.h>
 
+/* Structure containing level maps */
 typedef struct {
 	uint16_t map1[20];
 	uint16_t map2[20];
 	uint16_t map3[20];
-} maps_t;
+} Maps_t;
 
+/* Structure containing collectable level maps */
 typedef struct {
 	uint16_t empty[20];
 	uint16_t map1[20];
 	uint16_t map2[20];
 	uint16_t map3[20];
-	
-} map_collectables_t;
+} Map_collectables_t;
 
-/* Level stats */
+/* Level statistics */
 typedef struct {
     uint16_t level[20]; //Map
     uint16_t collectables[20]; //Collectable Map
     uint16_t X; // Player X ordinate
     uint16_t Y; // Player Y ordinate
-    uint8_t remainingCollectables; // Boss health
+    uint8_t remainingCollectables; // Collectables yet to ne obtained
 
 } Stats_t;
 
 
 /* Initialisation of level statistics */
 Stats_t level_init(uint16_t level[], uint16_t collectables[]);
+
+/* Initialises and returns the map for the specified level */
 uint16_t* map_get(uint8_t level);
-map_collectables_t collectables_init(void);
+
+/* Initialises and returns collectable maps for all levels */
+Map_collectables_t collectables_init(void);
 
 #endif
