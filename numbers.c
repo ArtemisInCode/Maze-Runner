7a3bc8b2dcfaede1b5a004d0e39bc2473ec4b1c4
Artemis Hingston <irh20@uclive.ac.nz>
2021-10-18 17:21:52 +1300

    Update numbers.c

--- numbers.c
+++ numbers.c
@@ -12,9 +12,9 @@
 void printNumbers(uint8_t number[], int8_t step)
 {
 
-			for(uint8_t i = 0; i<5; i++) { //write to each display columnn
-				pacer_wait(); //allow for physical switching time
-				if(i+step > 19 || i+step < 0) { //while in array bounds print number columns, 0x00 otherwise
+			for(uint8_t i = 0; i<5; i++) { // Write to each display columnn
+				pacer_wait(); // Allow for physical switching time
+				if(i+step > 19 || i+step < 0) { // While in array bounds print number columns, 0x00 otherwise
 					ledmat_display_column(0x00, i);
 				} else {
 					ledmat_display_column(number[i+step], i);
@@ -29,7 +29,7 @@ uint8_t* createNumber(uint16_t number, uint8_t* numLen)
 {
 	static uint8_t outputNumberArray[20];
 	
-	/*Array of bitmaps of all numbers 0 to 9*/
+	/* Array of bitmaps of all numbers 0 to 9 */
 	uint8_t numberArray[10][3] = {
 		{0x3e, 0x22, 0x3e},
 		{0x24, 0x3e, 0x20},
@@ -43,30 +43,30 @@ uint8_t* createNumber(uint16_t number, uint8_t* numLen)
 		{0x0e, 0x0a, 0x3e}
 		};
 		
-	uint8_t count = 5; //start position for scrolling
+	uint8_t count = 5; // Start position for scrolling
 	
-	for(uint8_t i = 0; i<20; i++) { //clears output
+	for(uint8_t i = 0; i<20; i++) { // Clears output
 		outputNumberArray[i] = 0x00;
 	}
 	
-	/*Splits number into digits and creates display bitmap*/
+	/* Splits number into digits and creates display bitmap */
 	while(number>0) {
 	
-		uint16_t mod = number % 10; //takes first digit of number
+		uint16_t mod = number % 10; // Takes first digit of number
 		
 		count--;
-		/*Adds zero column to front and adds columns of digit*/
+		/* Adds zero column to front and adds columns of digit */
 		outputNumberArray[count*4] = 0x00;
 		for(uint8_t i = ((count*4)+1); i<((count*4)+4); i++) {
 			outputNumberArray[i] = numberArray[mod][(i-((count*4)+1))];
 		}
 		
-		number = number/10; //removes first digit from number
+		number = number/10; // Removes first digit from number
 		
 	
 	
 	}
 	
-	*numLen = 5-count; //writes amount of numbers to be printed
-	return outputNumberArray; //returns bitmap to be diplayed
+	*numLen = 5-count; // Writes amount of numbers to be printed
+	return outputNumberArray; // Returns bitmap to be diplayed
 }
